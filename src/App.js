89fc28b9e82f91cca936bc24930ef7a312b78392
anthony_f <afanous@hotmail.com>
2023-07-11 11:47:27 -0400

    Added states

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from "react";
+import { useState } from "react";
 import Header from "./Header";
 import NotesList from "./Noteslist";
 
-const App = () => (
-  <div className="app">
-    <Header/>
-    <NotesList/>
-  </div>
-);
+const App = () => {
+  const [notes, setNotes] = useState([{
+  id: 0,
+  title: "eat",
+  description: "reese peanut butter cups",
+  doesMatchSearch: true
+},
+{
+  id: 1,
+  title: "sleep",
+  description: "eight hours",
+  doesMatchSearch: true
+},
+{
+  id: 2,
+  title: "code",
+  description: "build an awesome ui",
+  doesMatchSearch: true
+}]);
+
+const [searchText, setSearchText] = useState("Search for me");
+
+return (
+< div className="app" >
+  <Header />
+  <NotesList />
+</div >
+)
+};
 
 export default App;
